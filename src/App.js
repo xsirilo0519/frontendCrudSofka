4f06317e9ebf26f9bdd1813fa17cf350a80cc0b2
sebas <sebaslpez0519@gmail.com>
2021-11-24 17:25:45 -0500

    Create function list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useContext } from 'react'
 
+const HOST_API="http://localhost:4000/api"
+
+const List=()=>{
+  const {dispatch,state}=useContext(Store);
+  return <div>
+      <table>
+        <thead>
+          <tr>
+            <td>ID</td>
+            <td>Nombre</td>
+            <td>¿Esta completado?</td>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            state.List.map((todo)=>{
+              return <tr key={todo.id}>
+                <td>{todo.id}</td>
+                <td>{todo.name}</td>
+                <td>{todo.isCompleted}</td>
+              </tr>
+            })
+          }
+        </tbody>
+      </table>
+      </div>
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      
     </div>
   );
 }
