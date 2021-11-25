79fd9a3c0a2d0a2338bd0aebd0769cdfb7669d66
sebas <sebaslpez0519@gmail.com>
2021-11-24 20:45:41 -0500

    Ajustes hasta el video 3

--- src/App.js
+++ src/App.js
@@ -1,9 +1,56 @@
-import React, { useContext } from 'react'
+import React, { createContext, useContext, useEffect, useReducer, useRef, useState } from 'react'
 
-const HOST_API="http://localhost:4000/api"
+const HOST_API="http://localhost:8080/api"
+const initialState={
+  list: []
+}
+const Store =createContext(initialState)
+const Form=()=>{
+  const {dispatch}=useContext(Store);
+  const formref=useRef(null)
+  const [state,setState]=useState({});
+
+  const onAdd=(event) => {
+    event.preventDefault();
+
+    const request={
+      name:state.name,
+      id:null,
+      isComplete:false
+    }
+
+    fetch(HOST_API+"/todo",{
+      method:"POST",
+      body: JSON.stringify(request),
+      headers: {
+        'content-type':'application/json'
+      }
+    })
+    .then(response=>response.json())
+    .then((todo)=>{
+      dispatch({type: "add-item",item:todo});
+      setState({name:""});
+      formref.current.reset();
+    })
+  }
+
+  return <form ref={formref}>
+    <input type="text" name="name" onChange={(event)=>{
+      setState({...state,name: event.target.value})
+    }}></input>
+    <button onClick={onAdd}>Agregar</button>
+  </form>
+}
 
 const List=()=>{
   const {dispatch,state}=useContext(Store);
+useEffect(()=>{
+  fetch(HOST_API+"/todos")
+  .then(response=>response.json())
+  .then((list)=>{
+    dispatch({type:"update-list",list});
+  })
+},[state.list.length,dispatch])
   return <div>
       <table>
         <thead>
@@ -14,8 +61,7 @@ const List=()=>{
           </tr>
         </thead>
         <tbody>
-          {
-            state.List.map((todo)=>{
+          {state.list.map((todo)=>{
               return <tr key={todo.id}>
                 <td>{todo.id}</td>
                 <td>{todo.name}</td>
@@ -27,11 +73,34 @@ const List=()=>{
       </table>
       </div>
 }
+
+function reducer(state,action){
+  switch (action.type) {
+    case 'update-list':
+      return {...state, list:action.list}
+    case 'add-item':
+      const newList=state.list;
+      newList.push(action.item)
+      return {...state,list: newList}
+    default:
+      return {...state,list: []};
+  }
+}
+
+const StoreProvider=({children})=>{
+  const [state,dispatch]= useReducer(reducer,initialState)
+  return <Store.Provider value={{state,dispatch}}>
+    {children}
+  </Store.Provider>
+}
+
+
 function App() {
   return (
-    <div>
-      
-    </div>
+    <StoreProvider>
+      <Form/>
+      <List/>
+    </StoreProvider>
   );
 }
 
